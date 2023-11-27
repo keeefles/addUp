7a6c688c1bc80452f59699c69f9ffa55f8e092f2
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-27 12:51:47 +0200

    startin over

---
+++ script/code.js
@@ -0,0 +1,9 @@
+// let value1 = document.getElementById('value1').value;
+// let value2 = document.getElementById('value2').value;
+
+// let add = value1 + value2;
+//     document.querySelector('[data-output]').textContent = add;
+document.getElementById('add').addEventListener('click', function () {
+    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    
+});
