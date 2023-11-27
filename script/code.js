3e280b843ffea4ae0a6891b7ff02512693a5bb0a
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-27 14:11:13 +0200

    done

--- script/code.js
+++ script/code.js
@@ -3,7 +3,13 @@
 
 // let add = value1 + value2;
 //     document.querySelector('[data-output]').textContent = add;
-document.getElementById('add').addEventListener('click', function () {
+
+
+document.getElementById('add').addEventListener('click', function addUp () {
     let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    
-});
+    let add = 0;
+    array.forEach(num => {
+        add += num;
+    })
+    document.querySelector('[data-output]').textContent = add;
+});  
